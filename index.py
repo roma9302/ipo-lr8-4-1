ff1cb39e075725430b5d88bced96094b244a0c7a
roma <bshs71734@gmail.com>
2024-11-12 18:22:43 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,19 @@
-import json 
-
+import json
 
 # Загрузка данных из файла
-with open("strike.json", 'r', encoding='utf-8') as file: 
-    car_data = json.load(file) 
-
+with open("strike.json", 'r', encoding='utf-8') as file:
+    car_data = json.load(file)
 
 # Счетчик использований программы
-counter=0
+#Флаг закрытия программы
+counter = 0
 close = True
 
-#Вывод ошибок
+# Вывод ошибок
 def error(er):
-    print("Возникла ошибка." + " " + er)
- 
+    print("Возникла ошибка. "  + er)
 
-#меню программы
+# Меню программы
 def menu():
     print("""
        1: Вывести все записи n
@@ -24,9 +22,6 @@ def menu():
        4: Удалить запись по полю n
        5: Выйти из программы
         """)
-     
-
-
 
 # Вывод всего
 def out_all():
@@ -37,17 +32,14 @@ def out_all():
         Имя: {car['name']},                       
         Фабрика: {car['manufacturer']}, 
         Заправлена: {car['is_petrol']},    
-        Объем Бака: {car['tank_volume']} 
+        Объем Бакa: {car['tank_volume']} 
         """)
     counter += 1
 
-
-
-#Вывод по индексу 
+# Вывод по индексу
 def out_index():
     global counter
     idnum = int(input("Введите номер машины: "))
-    index = 0  
     for car in car_data:
         if idnum == car['id']:
             print(f"""
@@ -56,40 +48,35 @@ def out_index():
             Фабрика: {car['manufacturer']}, 
             Заправлена: {car['is_petrol']},    
             Объем Бака: {car['tank_volume']}
-            Индекс в списке: {index}
             """)
-            break  
-    else:
-        error("Машина не найдена")
+            counter += 1
+            return
+    error("Машина не найдена")
 
-        index += 1
-    counter += 1
-       
-
-#Ввод нового значения
+# Ввод нового значения
 def input_new():
-    global counter  
+    global counter
+    exists = False
     ids = int(input("Введите номер машины: "))
     
-    exists = False
     for car in car_data:
         if car['id'] == ids:
             exists = True
-            break
+        break
     
     if exists:
         error("Машина с таким номером уже существует.")
     else:
         name = input("Введите имя машины: ")  
         manufacturer = input("Введите завод изготовитель: ")  
-        is_petrol = input("Введите, заправлена ли машина (да/нет): ")  
+        is_petrol = input(" введите д/б == дизель/бензин ")  
         tank_volume = float(input("Введите объем бака машины: "))  
 
         new_car = {
             'id': ids,
             'name': name,
             'manufacturer': manufacturer,
-            'is_petrol': True if is_petrol.lower() == 'да' else False, 
+            'is_petrol': 'Дизель' if is_petrol == 'д' else 'Бензин', 
             'tank_volume': tank_volume
         }
 
@@ -97,60 +84,42 @@ def input_new():
         with open("strike.json", 'w', encoding='utf-8') as output_file: 
             json.dump(car_data, output_file, ensure_ascii=False, indent=2)
         print("Машина успешно добавлена.")
-    
-    counter += 1    
+        counter += 1    
 
-
-#Удаление по индексу
+# Удаление по индексу
 def del_id():
-      global counter
-      iddel = int(input("Введите номер машины: "))
-      yea = False  
-      for car in car_data:
+    global counter
+    iddel = int(input("Введите номер машины: "))
+    for car in car_data:
         if iddel == car['id']:
-            car_data.remove(car)  
-            yea = True  
-            break 
-        if yea:
+            car_data.remove(car)
             with open("strike.json", 'w', encoding='utf-8') as output_file:
                 json.dump(car_data, output_file, ensure_ascii=False, indent=2)
-                print("Машина успешно удалена.")
-        else:
-              error("Запись не найдена.")
-        counter += 1
+            print("Машина успешно удалена.")
+            counter += 1
+            return
+    error("Запись не найдена.")
 
-
-#Выход из программы
+# Выход 
 def leave():
-    global counter
     global close
     print(f"Программа завершена. Количество выполненных операций с записями равно: {counter}")
     close = False 
 
-
-#Главная функция
+# Главная функция
 def main():
     while close:
         menu()
-
         point = int(input("Введите номер действия, которое хотите выполнить: "))
 
         if point == 1:
             out_all()
-
-
         elif point == 2:
             out_index()
-
-
         elif point == 3:
             input_new()
-
-
         elif point == 4:
-           del_id()
-
-
+            del_id()
         elif point == 5:
             leave()
         else:
